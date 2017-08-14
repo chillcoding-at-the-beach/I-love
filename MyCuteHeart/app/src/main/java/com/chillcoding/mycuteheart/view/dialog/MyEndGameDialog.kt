7584703cbebad1da185852389adf4639af35d473
macha <macha@chillcoding.com>
2017-08-14 11:53:29 +0200

    Update to respect naming convention layout

--- MyCuteHeart/app/src/main/java/com/chillcoding/mycuteheart/view/dialog/MyEndGameDialog.kt
+++ MyCuteHeart/app/src/main/java/com/chillcoding/mycuteheart/view/dialog/MyEndGameDialog.kt
@@ -11,7 +11,7 @@ import com.chillcoding.mycuteheart.MyMainActivity
 import com.chillcoding.mycuteheart.R
 import com.chillcoding.mycuteheart.extension.DelegatesExt
 import com.chillcoding.mycuteheart.model.MyGameData
-import kotlinx.android.synthetic.main.dialog_end_game.view.*
+import kotlinx.android.synthetic.main.dialog_my_end_game.view.*
 import java.util.*
 
 /**
@@ -24,7 +24,7 @@ class MyEndGameDialog : DialogFragment() {
         var bestScore: Int by DelegatesExt.preference(activity, MyApp.PREF_BEST_SCORE, 0)
         val builder = AlertDialog.Builder(activity)
         val successString = resources.getStringArray(R.array.sucess_game)
-        var endGameView = (LayoutInflater.from(activity)).inflate(R.layout.dialog_end_game, null)
+        var endGameView = (LayoutInflater.from(activity)).inflate(R.layout.dialog_my_end_game, null)
         var data = arguments.getParcelable<MyGameData>(MyApp.BUNDLE_GAME_DATA)
         endGameView.dialogTitleTextView.text = "${successString[Random().nextInt(successString.size)].toUpperCase()}!"
         endGameView.dialogScoreTextView.text = "${data.score}"
