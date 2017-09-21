62629d7123bd4ddd93af8c9be4125771a65278fd
macha <macha@chillcoding.com>
2017-09-21 14:55:49 +0200

    Add awards fragment

---
+++ MyCuteHeart/app/src/main/java/com/chillcoding/mycuteheart/view/fragment/MyAwardsFragment.kt
@@ -0,0 +1,21 @@
+package com.chillcoding.mycuteheart.view.fragment
+
+import android.app.Fragment
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.chillcoding.mycuteheart.R
+
+/**
+ * Created by macha on 21/09/2017.
+ */
+class MyAwardsFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        var view = inflater?.inflate(R.layout.fragment_my_awards, container, false)
+        return view!!
+    }
+
+
+}
