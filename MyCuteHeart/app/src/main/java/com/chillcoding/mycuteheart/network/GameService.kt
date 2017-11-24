e1d41ef9e71304559d874aae44267ac592eab3ed
macha <macha@chillcoding.com>
2017-11-24 17:52:19 +0100

    Add the interface GameService
    This interface contain all the request to deal with server.
    Here we use `@GET` annotation for a GET request with the parameter `/scores`
    According to the server this GET request return a list of score
     in JSON format.
    Then after GET annotation we declare a function listScores without
    params, because the request no need params.
    The function listScores return an object Call of List<Score>,
    Every function related to a request return a Call object, then the Call object take in <> what the server return for the related request.

---
+++ MyCuteHeart/app/src/main/java/com/chillcoding/mycuteheart/network/GameService.kt
@@ -0,0 +1,14 @@
+package com.chillcoding.mycuteheart.network
+
+import com.chillcoding.mycuteheart.model.Score
+import retrofit2.Call
+import retrofit2.http.GET
+
+
+/**
+ * Created by macha on 24/11/2017.
+ */
+interface GameService {
+    @GET("/scores")
+    fun listScores(): Call<List<Score>>
+}
