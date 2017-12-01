00ccbd44b80bf6db1a71221adc72e68e2417a7b4
macha <macha@chillcoding.com>
2017-12-01 17:31:38 +0100

    Add object that represents table of scores

---
+++ MyCuteHeart/app/src/main/java/com/chillcoding/mycuteheart/db/Tables.kt
@@ -0,0 +1,13 @@
+package com.chillcoding.mycuteheart.db
+
+/**
+ * Created by macha on 01/12/2017.
+ */
+
+object ScoreTable {
+    val NAME = "Score"
+    val ID = "_id"
+    val SCORE = "score"
+    val PSEUDO = "pseudo"
+}
+
